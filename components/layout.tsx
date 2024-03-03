ba4aa52cae04d680b6387b7e7f5e5ad6f4f2438e
Yasir Idriz <yasiridriz@gmail.com>
2024-03-03 11:22:29 +0000

    added the wheel

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,129 +1,16 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, use } from "react";
 import Link from "next/link";
 import { PropsWithChildren } from "react";
-import axios from 'axios';
+import axios from "axios";
+import { Input } from "./ui/input";
+import { Button } from "./ui/button";
 
 const Layout = ({ children }: PropsWithChildren) => {
-  const [geolocation, setGeolocation] = useState<GeolocationCoordinates | null>(null);
-  const [locale, setLocale] = useState<string>("");
-  const [restaurants, setRestaurants] = useState<any[]>([]);
-
-  // useEffect(() => {
-  //   if (navigator.geolocation) {
-  //     navigator.geolocation.getCurrentPosition((position) => {
-  //       setGeolocation(position.coords);
-  //     });
-  //   }
-  // }, []);
-
-  const getCoordinatesToAddress = async (latitude : number, longitude : number) => {
-    const options = {
-      method: 'GET',
-      url: 'https://address-from-to-latitude-longitude.p.rapidapi.com/geolocationapi',
-      params: {
-        lat: latitude,
-        lng: longitude,
-      },
-      headers: {
-        'X-RapidAPI-Key': '090cd72862msh625177d2792eaf9p15b7b1jsndadbf6a4b3c6',
-        'X-RapidAPI-Host': 'address-from-to-latitude-longitude.p.rapidapi.com'
-      }
-     };
-
-      try {
-        const response = await axios.request(options);
-        console.log(response.data);
-        return response.data;
-      } catch (error) {
-        console.error(error);
-      }
-      
-    };
-
-  const handleTryIt = async () => {
-    if (navigator.geolocation) {
-
-      navigator.geolocation.getCurrentPosition(async (position) => {
-        console.log('navigator.geolocation: ',  position )
-
-        setGeolocation(position.coords);
-        const address = await getCoordinatesToAddress(position.coords.latitude, position.coords.longitude);
-        const addres = address.Results[0].address;
-        const latt = address.Results[0].latitude;
-        const longg = address.Results[0].longitude;
-        const city = address.Results[0].subregion;
-        setLocale(addres);
   
-        console.log('Address:', address);
-        console.log(addres);
-
-        const options = {
-          method: 'GET',
-          url: 'https://the-fork-the-spoon.p.rapidapi.com/restaurants/v2/auto-complete',
-          params: {
-            text: city,
-            latitude: latt,
-            longitude: longg,
-          },
-          headers: {
-            'X-RapidAPI-Key': '090cd72862msh625177d2792eaf9p15b7b1jsndadbf6a4b3c6',
-            'X-RapidAPI-Host': 'the-fork-the-spoon.p.rapidapi.com'
-          }
-        };
-        
-        try {
-          const response = await axios.request(options);
-          console.log(response.data);
-          const restaurants  = response.data.autocomplete;
-          restaurants.forEach((item: any) => {
-            if (item.name && item.name.text) {
-              console.log(item.name.text);
-            }
-          });
-        } catch (error) {
-          console.error(error);
-        }
-      
-      });
-    } else {
-      setGeolocation(null);
-    }
-  };
 
   return (
     <div>
-      <div className="fixed top-0 my-2 flex justify-center items-center gap-3 w-full font-bold">
-        <Link href={"/"}>No waste</Link>
-        <Link href={"/dashboard"}>Dashboard</Link>
-        <Link href={"/about"}>Who are we?</Link>
-      </div>
-      
-
-      <main className="px-[15vw] pt-20">
-        <h1>HTML Geolocation</h1>
-
-        {geolocation ? (
-          <p>
-            Latitude: {geolocation.latitude}, Longitude:{" "}
-            {geolocation.longitude}
-          </p>
-        ) : (
-          <p>Click the button to get your coordinates.</p>
-        )}
-        <p> You address is : {locale} </p>
-
-        <button onClick={handleTryIt}>Try It</button>
-
-        <div id="demo">
-          {restaurants.map((r: any) => {
-            return (
-              <div>
-                {r}
-              </div>
-            )
-          })}
-        </div>
-
+      <main className="mx-auto pt-20 px-3 max-h-[100dvh] overflow-y-hidden flex justify-center items-center max-w-xl">
         {children}
       </main>
     </div>
@@ -131,4 +18,3 @@ const Layout = ({ children }: PropsWithChildren) => {
 };
 
 export default Layout;
-
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,207 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import axios from "axios";
+import { useEffect, useState } from "react";
+//@ts-ignore
+import WheelComponent from "react-wheel-of-prizes";
 
 const inter = Inter({ subsets: ["latin"] });
 
+const segColors = [
+  "#EE4040",
+  "#F0CF50",
+  "#815CD1",
+  "#3DA5E0",
+  "#34A24F",
+  "#F9AA1F",
+  "#EC3F3F",
+  "#FF9000",
+];
+
 export default function Home() {
-  return <div></div>;
+  const [isLoading, setIsLoading] = useState(false);
+  const [geolocation, setGeolocation] = useState<GeolocationCoordinates | null>(
+    null
+  );
+  const [location, setLocation] = useState<any>();
+  const [locale, setLocale] = useState<any>();
+  const [restaurants, setRestaurants] = useState<any[]>();
+  const [city, setCity] = useState<any>("");
+
+  const position = async () => {
+    await navigator.geolocation.getCurrentPosition(
+      (position) => {
+        alert("Latitude: " + position.coords.latitude);
+        setLocation({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+        });
+      },
+      (err) => console.error(err)
+    );
+  };
+
+  useEffect(() => {
+    position();
+  }, []);
+
+  useEffect(() => {
+    if (location) {
+      console.log(location);
+      getCoordinatesToAddress(location.latitude, location.longitude);
+    }
+  }, [location]);
+
+  const onFinished = (winner: any) => {
+    console.log(winner);
+  };
+
+  const getCoordinatesToAddress = async (
+    latitude: number,
+    longitude: number
+  ) => {
+    const options = {
+      method: "GET",
+      url: "https://address-from-to-latitude-longitude.p.rapidapi.com/geolocationapi",
+      params: {
+        lat: latitude,
+        lng: longitude,
+      },
+      headers: {
+        "X-RapidAPI-Key": "090cd72862msh625177d2792eaf9p15b7b1jsndadbf6a4b3c6",
+        "X-RapidAPI-Host": "address-from-to-latitude-longitude.p.rapidapi.com",
+      },
+    };
+
+    try {
+      const response = await axios.request(options);
+      console.log(response.data);
+      return response.data;
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleSearch = async () => {
+    setIsLoading(true);
+
+    const options = {
+      method: "GET",
+      url: "https://the-fork-the-spoon.p.rapidapi.com/locations/v2/auto-complete",
+      params: { text: city },
+      headers: {
+        "X-RapidAPI-Key": "090cd72862msh625177d2792eaf9p15b7b1jsndadbf6a4b3c6",
+        "X-RapidAPI-Host": "the-fork-the-spoon.p.rapidapi.com",
+      },
+    };
+    await axios
+      .request(options)
+      .then(async (res) => {
+        let geoId = res.data.data.geolocation[0].id.id;
+        let geoText = res.data.data.geolocation[0].name.text;
+        console.log(geoId, geoText);
+        setCity(res.data.data.geolocation[0].name.text);
+
+        if (geoId) {
+          const options = {
+            method: "GET",
+            url: "https://the-fork-the-spoon.p.rapidapi.com/locations/v2/list",
+            params: {
+              google_place_id: geoId,
+              geo_ref: "false",
+              geo_type: "locality",
+            },
+            headers: {
+              "X-RapidAPI-Key":
+                "51b69c5bbemshb17cd3a995ba2d8p17ce34jsn8f18cd88291a",
+              "X-RapidAPI-Host": "the-fork-the-spoon.p.rapidapi.com",
+            },
+          };
+
+          axios
+            .request(options)
+            .then((location) => {
+              let cityId = location.data.id_city;
+              const roptions = {
+                method: "GET",
+                url: "https://the-fork-the-spoon.p.rapidapi.com/restaurants/v2/list",
+                params: {
+                  queryPlaceValueCityId: cityId,
+                  pageSize: "8",
+                  pageNumber: "1",
+                  queryPlaceValueCoordinatesLatitude: "51.5072178",
+                  queryPlaceValueCoordinatesLongitude: "-0.1275862",
+                },
+                headers: {
+                  "X-RapidAPI-Key":
+                    "51b69c5bbemshb17cd3a995ba2d8p17ce34jsn8f18cd88291a",
+                  "X-RapidAPI-Host": "the-fork-the-spoon.p.rapidapi.com",
+                },
+              };
+              axios
+                .request(roptions)
+                .then((restaurants) => {
+                  console.log(restaurants.data);
+                  setRestaurants(restaurants.data.data.map((r: any) => r.name));
+                  setIsLoading(false);
+                })
+                .catch((error) => {
+                  console.error(error);
+                });
+            })
+            .catch((error) => {
+              console.error(error);
+            });
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+        setIsLoading(false);
+      });
+  };
+
+  return (
+    <div className="w-full">
+      {!location && (
+        <div className="w-full">
+          Type in your city
+          <div className="flex gap-2 justify-between my-2">
+            <Input
+              onChange={(e) => setCity(e.target.value)}
+              type="text"
+              className="w-full"
+              placeholder="Your city"
+              required
+            />
+            <Button
+              onClick={handleSearch}
+              disabled={isLoading || !(city.length > 0)}
+            >
+              Search
+            </Button>
+          </div>
+          {isLoading && <p>Loading...</p>}
+        </div>
+      )}
+      <div className="overflow-x-hidden pl-1">
+        {restaurants && (
+          <WheelComponent
+            segments={restaurants}
+            segColors={segColors}
+            onFinished={(winner: any) => onFinished(winner)}
+            primaryColor="black"
+            contrastColor="white"
+            buttonText="Spin"
+            className="p-0"
+            isOnlyOnce={false}
+            upDuration={100}
+            downDuration={1000}
+            fontFamily="Arial"
+          />
+        )}
+      </div>
+    </div>
+  );
 }
